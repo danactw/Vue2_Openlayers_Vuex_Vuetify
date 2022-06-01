dfb639ad5618f6d87918923eb8fa2f15d8aa5b73
danachen <danac.tw@gmail.com>
2022-06-01 14:01:51 +0800

    switch base layers

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,40 +1,44 @@
 <template>
   <div>
     <v-navigation-drawer app clipped>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Application
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
       <v-list
         dense
         nav
       >
-        <v-list-item-group
-          v-model="currentBaseLayer"
-          active-class=""
-        >
-          <v-list-item
-            v-for="baseLayer in baseLayersTitle"
-            :key="baseLayer"
-            link
-          >
-            <template v-slot:default="{ active }">
-              <v-list-item-action>
-                <v-checkbox :input-value="active"></v-checkbox>
-              </v-list-item-action>
-
-              <v-list-item-content>
-                <v-list-item-title> {{ baseLayer }} </v-list-item-title>
-              </v-list-item-content>
-            </template>
+      <v-hover>
+        <v-subheader class="text-h6 baseLayer" @click="showBaseLayer=!showBaseLayer">Base Layer</v-subheader>
+      </v-hover>
+      <v-radio-group v-model="currentBaseLayer" v-show="showBaseLayer">      
+        <v-radio
+          v-for="baseLayer in baseLayersTitle"
+          :key="baseLayer"
+          :label="baseLayer"
+          :value="baseLayer"
+        ></v-radio>
+      </v-radio-group>
+      </v-list>
+      <v-list v-show="currentBaseLayer === 'Bing Map'">
+        <v-list-item>
+          <v-select
+            v-model="currentBingMap"
+            :items="BingMapstyles"
+            label="Bing Map"
+            single-line
+          ></v-select>
           </v-list-item>
-        </v-list-item-group>
+      </v-list>
+      <v-list>
+        <v-subheader class="text-h6">Base Layer Opacity</v-subheader>
+        <v-list-item v-show="showBaseLayer">
+            <v-slider
+              v-model="baseLayerOpacity"
+              step="0.1"
+              min="0"
+              max="1"
+              thumb-label
+              ticks
+            ></v-slider>
+          </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <div id="map" class="map" ref="mapContainer"></div>
@@ -57,9 +61,14 @@ export default {
     return {
       mapContainer: null,
       map: null,
+      showBaseLayer:false,
       baseLayers: [],
       baseLayersTitle: [],
-      currentBaseLayer: 0
+      currentBaseLayer: "OSM Standard",
+      baseLayerOpacity:1,
+      BingMapstyles: [ 'RoadOnDemand', 'AerialWithLabelsOnDemand', 'CanvasDark' ],
+      BingMaps: [],
+      currentBingMap: 'RoadOnDemand'
     }
   },
   methods: {
@@ -100,11 +109,27 @@ export default {
         zIndex: 0,
         title: "Stamen Water"
       });
-      this.baseLayers = [OSMStandard, OSMHumanitarian, CartoDBBase, StamenWater ]
+      for (let i = 0; i < this.BingMapstyles.length ; i++ ) {
+        this.BingMaps.push(
+          new TileLayer({
+            source: new BingMaps({
+              key: "Ak6hRbflYvuARcql-hA_RTu20jkC6UQGDi4PLkJ1vOHrD30DD-1et3-N3DSohoPA",
+              imagerySet: this.BingMapstyles[i]
+            }),
+            visible: false,
+            opacity: 0,
+            zIndex: 0,
+            title: "Bing Map"
+          })
+        )
+      }
+      this.baseLayers = [OSMStandard, OSMHumanitarian, CartoDBBase, StamenWater, ...this.BingMaps ]
       const baseLayerGroup = new LayerGroup({
           layers: this.baseLayers
       });
-      this.baseLayers.forEach(layer => this.baseLayersTitle.push(layer.get('title')))
+      this.baseLayers.forEach(layer => {
+        if (!this.baseLayersTitle.includes(layer.get('title'))) this.baseLayersTitle.push(layer.get('title'))
+      })
       this.map = new Map({
         layers: [
           baseLayerGroup
@@ -115,12 +140,41 @@ export default {
           zoom: 2,
         }),
       })
+    },
+  },
+  watch: {
+    currentBaseLayer () {
+      this.map.getLayers().getArray()[0].getLayers().getArray().forEach(layer => {
+        if (layer.get('title') === this.currentBaseLayer) {
+          if (layer.get('title') === 'Bing Map') {
+            this.BingMaps.forEach(style => {
+              style.setVisible(style.getSource().getImagerySet()==='RoadOnDemand')
+              style.setOpacity(1)
+            })
+          } else {
+            layer.setVisible(true)
+            layer.setOpacity(1)
+          }
+          this.baseLayerOpacity = 1
+        } else layer.setVisible(false)
+      })
+    },
+    baseLayerOpacity() {
+      this.map.getLayers().getArray()[0].getLayers().getArray().forEach(layer => {
+        if (layer.get('title') === this.currentBaseLayer) layer.setOpacity(this.baseLayerOpacity)
+      })
+    },
+    currentBingMap() {
+      this.BingMaps.forEach(style => {
+        style.setVisible(style.getSource().getImagerySet()===this.currentBingMap)
+        style.setOpacity(1)
+        this.baseLayerOpacity = 1
+      })
     }
   },
   mounted() {
     this.initMap()
-  },
-  
+  }
 }
 </script>
 
@@ -128,5 +182,8 @@ export default {
 .map {
   width: 100vw;
   height: 100vh;
+}
+.v-subheader.baseLayer{
+  cursor: pointer;
 }
 </style>
