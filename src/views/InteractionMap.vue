3e94bcb770c5f8423f8420662623faef6659bf96
danachen <danac.tw@gmail.com>
2022-06-06 14:25:52 +0800

    switch interactions

--- src/views/InteractionMap.vue
+++ src/views/InteractionMap.vue
@@ -48,170 +48,264 @@ import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import { OSM, Vector as VectorSource } from 'ol/source';
 import GeoJSON from 'ol/format/GeoJSON';
 import { Draw, Modify, Select, Translate, Snap } from 'ol/interaction';
-import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import { Circle as CircleStyle, Fill, Stroke, Style, Text, RegularShape } from 'ol/style';
+import { createRegularPolygon, createBox} from 'ol/interaction/Draw';
+import { getLength, getArea } from 'ol/sphere';
+import { Point, LineString } from 'ol/geom';
 import InputRadio from '@/components/InputRadio.vue';
 import InputCheckbox from '@/components/InputCheckbox.vue';
 
 export default {
-    data() {
-        return {
-            mapContainer: null,
-            map: null,
-            interactionsType: ['Draw', 'Translate', 'Modify', 'Scale and Rotate'],
-            drawType: ['Point', 'LineString', 'Circle', 'Regular Polygon', 'Rectangle', 'Polygon(freehand)'],
-            RegularPolygonSize: 3,
-            addtional: ['Measure', 'Measure Segment Length', 'Clear Previous Feature'],
-            geojsonObject: {
-                "type": "FeatureCollection",
-                "crs": {
-                    "type": "name",
-                    "properties": {
-                        "name": "EPSG:3857",
-                    },
-                },
-                "features": [
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Polygon",
-                            "coordinates": [
-                                [
-                                    [-5000000, 6000000],
-                                    [-5000000, 8000000],
-                                    [-3000000, 8000000],
-                                    [-3000000, 6000000],
-                                    [-5000000, 6000000],
-                                ],
-                            ],
-                        },
-                    },
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Polygon",
-                            "coordinates": [
-                                [
-                                    [-2000000, 6000000],
-                                    [-2000000, 8000000],
-                                    [0, 8000000],
-                                    [0, 6000000],
-                                    [-2000000, 6000000],
-                                ],
-                            ],
-                        },
-                    },
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Polygon",
-                            "coordinates": [
-                                [
-                                    [1000000, 6000000],
-                                    [1000000, 8000000],
-                                    [3000000, 8000000],
-                                    [3000000, 6000000],
-                                    [1000000, 6000000],
-                                ],
-                            ],
-                        },
-                    },
-                    {
-                        "type": "Feature",
-                        "geometry": {
-                            "type": "Polygon",
-                            "coordinates": [
-                                [
-                                    [-2000000, -1000000],
-                                    [-1000000, 1000000],
-                                    [0, -1000000],
-                                    [-2000000, -1000000],
-                                ],
-                            ],
-                        },
-                    },
+  data() {
+    return {
+      mapContainer: null,
+      map: null,
+      newFeature: null,
+      interactionsType: ['Draw', 'Translate', 'Modify', 'Scale and Rotate'],
+      draw: null,
+      translate: null,
+      modify: null,
+      snap: null,
+      drawType: ['Point', 'LineString', 'Circle', 'Regular Polygon', 'Rectangle', 'Polygon(freehand)'],
+      RegularPolygonSize: 3,
+      addtional: ['Measure', 'Measure Segment Length', 'Clear Previous Feature'],
+      geojsonObject: {
+        "type": "FeatureCollection",
+        "crs": {
+          "type": "name",
+          "properties": {
+            "name": "EPSG:3857",
+          },
+        },
+        "features": [
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [-5000000, 6000000],
+                  [-5000000, 8000000],
+                  [-3000000, 8000000],
+                  [-3000000, 6000000],
+                  [-5000000, 6000000],
                 ],
+              ],
             },
-            drawStyle: new Style({
-                fill: new Fill({
-                    color: "rgba(255, 255, 255, 0.2)",
-                }),
-                stroke: new Stroke({
-                    color: "#ffcc33",
-                    width: 2,
-                }),
-                image: new CircleStyle({
-                    radius: 7,
-                    fill: new Fill({
-                        color: "#ffcc33",
-                    }),
-                }),
-            }),
-        };
-    },
-    methods: {
-        initMap() {
-            const source = new VectorSource({
-                features: new GeoJSON().readFeatures(this.geojsonObject),
-            });
-            const USLayer = new VectorLayer({
-                source: new VectorSource({
-                    url: "https://openlayers.org/data/vector/us-states.json",
-                    format: new GeoJSON(),
-                    wrapX: false,
-                })
-            });
-            const newFeature = new VectorLayer({
-                source: source,
-                style: this.drawStyle,
-            });
-            let draw;
-            function addInteractions() {
-                draw = new Draw({
-                    source: newFeature.getSource(),
-                    type: "Polygon",
-                });
-                if (map.value)
-                    map.value.addInteraction(draw);
-            }
-            addInteractions();
-            const select = new Select({
-                wrapX: false,
-            });
-            const translate = new Translate({
-                features: select.getFeatures(),
-            });
-            const modify = new Modify({
-                features: select.getFeatures(),
-            });
-            const snap = new Snap({
-                source: newFeature.getSource(),
-                pixelTolerance: 15
-            });
-            this.map = new Map({
-                layers: [
-                    new TileLayer({
-                        source: new OSM(),
-                    }),
-                    newFeature,
-                    USLayer
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [-2000000, 6000000],
+                  [-2000000, 8000000],
+                  [0, 8000000],
+                  [0, 6000000],
+                  [-2000000, 6000000],
                 ],
-                target: "map",
-                view: new View({
-                    center: [0, 0],
-                    zoom: 2,
-                }),
-            });
-            this.map.addInteraction(select);
-            // this.map.addInteraction(draw)
-            this.map.addInteraction(translate);
-            this.map.addInteraction(modify);
-            this.map.addInteraction(snap);
+              ],
+            },
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [1000000, 6000000],
+                  [1000000, 8000000],
+                  [3000000, 8000000],
+                  [3000000, 6000000],
+                  [1000000, 6000000],
+                ],
+              ],
+            },
+          },
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Polygon",
+              "coordinates": [
+                [
+                  [-2000000, -1000000],
+                  [-1000000, 1000000],
+                  [0, -1000000],
+                  [-2000000, -1000000],
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      vectorStyle: {
+        'Point': new Style({
+          image: new CircleStyle({
+            radius: 5,
+            fill: new Fill({
+              color: '#ffcc33',
+            }),
+          })
+        }),
+        'LineString': new Style({
+          stroke: new Stroke({
+            color: 'blue',
+            width: 2
+          })
+        }),
+        'Polygon': new Style({
+          fill: new Fill({
+            color: 'rgba(0, 153, 0, 0.3)'
+          }),
+          stroke: new Stroke({
+            color: 'rgba(0, 153, 0,1)',
+            width: 2
+          }),
+        }),
+        'Circle': new Style({
+          fill: new Fill({
+            color: 'rgba(255, 128, 128,0.5)'
+          }),
+          stroke: new Stroke({
+            color: 'rgba(255, 128, 128,1)',
+            width: 2
+          }),
+        })
+      },
+    };
+  },
+  methods: {
+    initMap() {
+      const source = new VectorSource({
+        features: new GeoJSON().readFeatures(this.geojsonObject),
+      });
+      const USLayer = new VectorLayer({
+        source: new VectorSource({
+          url: "https://openlayers.org/data/vector/us-states.json",
+          format: new GeoJSON(),
+          wrapX: false,
+        })
+      });
+      this.newFeature = new VectorLayer({
+        source: source,
+        style: feature => this.styleFunction(feature),
+      });
+      const select = new Select({
+        wrapX: false,
+      });
+      this.translate = new Translate({
+        features: select.getFeatures(),
+      });
+      this.modify = new Modify({
+        features: select.getFeatures(),
+      });
+      this.snap = new Snap({
+        source: this.newFeature.getSource(),
+        pixelTolerance: 15
+      });
+      this.map = new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+          this.newFeature,
+          USLayer
+        ],
+        target: "map",
+        view: new View({
+          center: [0, 0],
+          zoom: 2,
+        }),
+      });
+      this.map.addInteraction(select);
+      this.map.addInteraction(this.translate);
+    },
+    styleFunction(feature, showHint) {
+      const geometry = feature.getGeometry();
+      const type = geometry.getType();
+      const style = [this.vectorStyle[type]]
+      // let drawType, measureOutput, measureOutputCoord, segmentOutputCoord;
+      return style
+    },
+    addDraw() {
+      let type, geometryFunction
+      switch (this.$store.state.map.selectedDrawType) {
+        case 'Point':
+          type = 'Point'
+          break;
+        case 'LineString':
+          type = 'LineString'
+          break;
+        case 'Polygon(freehand)':
+          type = 'Polygon'
+          break;
+        case 'Rectangle':
+          type = 'Circle'
+          geometryFunction = createBox()
+          break;
+        case 'Circle':
+          type = 'Circle'
+          break;
+        case 'Regular Polygon':
+          type = 'Circle'
+          geometryFunction = createRegularPolygon(Number(this.RegularPolygonSize))
+          break;
+      }
+      this.draw = new Draw({
+        type: type,
+        source: this.newFeature.getSource(),
+        style: feature => this.styleFunction(feature),
+        geometryFunction: geometryFunction
+      })
+      // this.draw.on('drawstart', drawStart)
+      // this.draw.on('drawend', drawEnd)
+      this.map.addInteraction(this.draw)
+    },
+    removeAllInteractions () {
+      this.map.removeInteraction(this.draw)
+      this.map.removeInteraction(this.translate)
+      this.map.removeInteraction(this.modify)
+      this.map.removeInteraction(this.snap)
+    }
+  },
+  mounted() {
+    this.initMap()
+  },
+  created() {
+    this.$store.watch(
+      state => state.map.selectedinteraction,
+      (newValue, oldValue) => {
+        this.removeAllInteractions()
+        switch(newValue) {
+          case 'Draw':
+            this.addDraw()
+            break;
+          case 'Translate':
+            this.map.addInteraction(this.translate);
+            break;
+          case 'Modify':
+            this.map.addInteraction(this.modify);
+            this.map.addInteraction(this.snap);
+            break;
         }
-    },
-    mounted() {
-        this.initMap();
-    },
-    components: { InputRadio, InputRadio, InputCheckbox }
+      }
+    ),
+    this.$store.watch(
+      state => state.map.selectedDrawType,
+      () => {
+        this.map.removeInteraction(this.draw)
+        this.addDraw()
+      }
+    )
+  },
+  watch: {
+    RegularPolygonSize() {
+      this.map.removeInteraction(this.draw)
+      this.addDraw()
+    }
+  },
+  components: { InputRadio, InputCheckbox }
 }
 </script>
 
